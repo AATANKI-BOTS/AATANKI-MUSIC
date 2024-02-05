db53b14be1ae3e8f27f9c1d102b9fdac89534b3c
AATANKI-BOTS <159051683+AATANKI-BOTS@users.noreply.github.com>
2024-02-06 00:54:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Lily_x_bots")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+mBKAuzF9eP05ZTM1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+-2grMhOKoww5Nzg1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+aih6DZ8qerdlMWU9")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/9592477735faa34267c71.jpg)"
+    "START_IMG_URL", "https://graph.org/file/4b11c10cf7c7bdf1b5fe5.jpg)"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://graph.org/file/47605a2381cd25cc8b050.jpg"
