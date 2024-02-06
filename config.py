da4685bdbb5a20db081c60a63586b712c34337ee
AATANKI-BOTS <159051683+AATANKI-BOTS@users.noreply.github.com>
2024-02-06 12:25:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,9 +32,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/venomXbots/Lilyrobot",
+    "https://github.com/AATANKI-BOTS/AATANKI-MUSIC",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "patch-1")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
